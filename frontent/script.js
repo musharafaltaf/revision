412e81c73a1b81b3ec0a1c2c7ea6dae3daac3569
Musharaf Altaf <musharafdar10@Gmail.com>
2026-01-27 23:03:14 +0530

    trying

---
+++ frontent/script.js
@@ -0,0 +1,74 @@
+console.log("hello world")
+console.log("wakeens kya chuk kara")
+
+
+console.log(5+3);
+console.log("5"+3);
+console.log("5"-3);
+
+
+
+let person ={
+    name : "musharaf",
+    fathers_name:"altaf hussain dar",
+    mothers_name:"hajira begum",
+    subject : "javascript",
+    class: "2nd year",
+    age : 21
+
+}
+console.log(person.subject);
+
+let selectcolors = ["red","yellow","black"]
+selectcolors[3]="white";
+console.log(selectcolors.length);
+
+
+function greet(name) {
+    console.log("hello " + name);
+}
+greet('musharaf');
+
+function greet(aa){
+    console.log("cxe kyu kara "+ aa);
+}
+greet('musharafa')
+
+
+let a = 10;
+let b = 20;
+let operations = "+";
+let result;
+
+if (operations === "-"){
+    result = a-b;
+} else if (operations === "+"){
+    result = a+b;
+} else if (operations === "*"){
+    result = a*b;
+} else if (operations === "%"){
+    result = a%b;
+} else {
+    console.log("invalid operations");
+}
+console.log(result);
+
+
+function calculator(a,b,op){
+    a : 30;
+    b : 40;
+    op : "*";
+    result;
+
+    
+}
+if(op === "+"){
+    result=a+b;
+} else if (op ==="-"){
+    result = a-b;
+} else if (op === "*"){
+    result = a*b;
+} else {
+    console.log("invalid op")
+}
+console.log(result)
